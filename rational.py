de6c8b8534ad0d491979feee654b333244fcc73c
Kirill Zlobinov <zlobinovk@yandex.ru>
2025-09-15 17:32:02 +0300

    1

--- rational.py
+++ rational.py
@@ -1,1 +1,14 @@
-def
+def make(numer, denom):
+    numer, denom = normalize(numer, denom)
+    return {'numer': numer, 'denom': denom}
+
+
+def normalize(numer, denom):
+    gcd = math.gcd(numer, denom)
+    return numer // gcd, denom // gcd
+
+
+def to_common_denom(rat1, rat2):
+    denom1 = rat1['denom']
+    denom2 = rat2['denom']
+    lcm = abs(denom1 * denom2) // math.gcd(denom1, denom2)
