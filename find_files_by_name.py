643bf599376788aa38bd176e5d3c5339e270255a
Kirill Zlobinov <zlobinovk@yandex.ru>
2025-09-19 21:42:07 +0300

    find_files_by_name

---
+++ find_files_by_name.py
@@ -0,0 +1,11 @@
+def find_files_by_name(node, substr):
+    def walk(node, substr, ancestry):
+        name = get_name(node)
+        ancestry.append(name)
+        if is_file(node) and (substr in name):
+            return [os.path.join(ancestry)]
+        elif is_file(node):
+            return []
+        return [walk(child, substr, ancestry[:]) for child in get_children(node)] 
+
+    return flattern(walk(node, substr, []))
