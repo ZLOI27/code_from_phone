5b82fd8fc4b50066da7871b9ef8830962c5e692e
Zlobinov Kirill <47040912+ZLOI27@users.noreply.github.com>
2025-09-24 23:46:46 +0300

    Update validate.py

--- validate.py
+++ validate.py
@@ -1,16 +1,16 @@
-def __init__(self,
-             min_len=self.OPTIONS['min_len'],
-             contain_numbers=self.OPTIONS['contain_numbers'],
+    def __init__(self,
+             min_len=OPTIONS['min_len'],
+             contain_numbers=OPTIONS['contain_numbers'],
              **kwargs
              ):
-    self.min_len = min_len
-    self.contain_numbers = contain_numbers
+        self.min_len = min_len
+        self.contain_numbers = contain_numbers
 
 
-def validate(password: str) -> dict:
-    result = {}
-    if len(password) < self.min_len:
-        result['min_len'] = 'too small'
-    elif _has_number(password):
-        result['contain_numbers'] = 'should contain at least one number'
-    return result
+    def validate(self, password: str) -> dict:
+        result = {}
+        if len(password) < self.min_len:
+            result['min_len'] = 'too small'
+        if not self._has_number(password) and self.contain_numbers:
+            result['contain_numbers'] = 'should contain at least one number'
+        return result
