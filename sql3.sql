84d92e9f79164b8e2f337c1ba3f14ce8605e0b44
Zlobinov Kirill <47040912+ZLOI27@users.noreply.github.com>
2025-10-30 20:05:43 +0300

    Update sql3.sql

--- sql3.sql
+++ sql3.sql
@@ -2,13 +2,13 @@ CREATE TABLE users (
 	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	name VARCHAR(30)
 );
-INSERT INTO users (name) VALUES ("Tom");
+INSERT INTO users (name) VALUES ('Tom');
 CREATE TABLE cars (
 	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	user_id INT,
 	model VARCHAR(20)
 );
 INSERT INTO cars (user_id, model) VALUES
-(1, "Volvo"),
-(1, "Mercedes");
+(1, 'Volvo'),
+(1, 'Mercedes');
 
